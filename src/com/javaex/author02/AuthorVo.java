f70bec6cb2af1103044a3ce98120a92412a76a2b
teauk <teauk@DESKTOP-9OJS59T.local>
2020-12-30 11:21:32 +0900

    author02 2020-12-30

---
+++ src/com/javaex/author02/AuthorVo.java
@@ -0,0 +1,54 @@
+package com.javaex.author02;
+
+public class AuthorVo {
+	//필드
+	public int authorId;
+	public String authorName;
+	public String authordesc;
+	//생성자 
+	public AuthorVo() {}
+	
+	public AuthorVo(String authorName, String authordesc) {	
+		this.authorName = authorName;
+		this.authordesc = authordesc;
+	}
+	public AuthorVo(int authorId, String authorName, String authordesc) {
+		
+		this.authorId = authorId;
+		this.authorName = authorName;
+		this.authordesc = authordesc;
+	}
+
+	//메소드 getset
+	public int getAuthorId() {
+		return authorId;
+	}
+	public void setAuthorId(int authorId) {
+		this.authorId = authorId;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+	public String getAuthordesc() {
+		return authordesc;
+	}
+	public void setAuthordesc(String authordesc) {
+		this.authordesc = authordesc;
+	}
+	
+	@Override
+	public String toString() {
+		return "AuthorVo [authorId=" + authorId + ", authorName=" + authorName + ", authordesc=" + authordesc + "]";
+	}
+	
+	
+	//메소드 일반
+	
+	
+	
+	
+	
+}
